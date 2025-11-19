f6d154061c986a0a8d8f371aecdb9ec535f412bf
n2i-nishi <82163707+N-i-ke@users.noreply.github.com>
2025-11-19 22:13:38 +0900

    refactor: change directory

---
+++ react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@wabi-ui/react";
+
+export default function App() {
+  return (
+    <>
+      <Button onClick={() => alert("clicked")}>Hello from wabi Button</Button>
+    </>
+  );
+}
+
+
