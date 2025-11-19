3790ccd5ce08ea737ca0531c141361a6fa5e833a
n2i-nishi <82163707+N-i-ke@users.noreply.github.com>
2025-11-19 21:54:26 +0900

    fix: first UI button words

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -3,10 +3,9 @@ import { Button } from "@wabi-ui/react";
 
 export default function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>wabi-ui — examples</h1>
-      <Button onClick={() => alert("clicked")}>Hello from wabi Button</Button>
-    </div>
+    <>
+      <Button onClick={() => alert("clicked")}>wabi Button</Button>
+    </>
   );
 }
 
